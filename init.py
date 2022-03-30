fefbec5df1422bb190d1cc03b51c32c3c8f6defa
Manske <lemuelkaue@gmail.com>
2022-03-30 10:53:51 -0300

    Update init.py

--- init.py
+++ init.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask('lindo sabado letivo estou aqui escrevendo isso')
-
+app = Flask()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 db = SQLAlchemy(app)
