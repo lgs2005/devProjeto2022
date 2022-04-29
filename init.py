72d1d116a7de727614361b982c8a6c4a8426725c
sem <>
2022-04-29 16:54:36 -0300

    dar nome ao projeto 2

--- init.py
+++ init.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask("projeto")
+app = Flask("projeto 2")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
