dc511146690b5855ba0273f8ae624b6698228f66
sem <>
2022-04-29 16:36:48 -0300

    mudei o nome do projeto

--- init.py
+++ init.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask("nome")
+app = Flask("projeto")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
