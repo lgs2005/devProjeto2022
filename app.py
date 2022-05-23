6b6ac117ac6579f76d2df66c9f4581b5850ae3bf
Lemuel Manske <lemuelkaue@gmail.com>
2022-05-23 18:05:44 -0300

    Removendo coisas desnecessárias

--- app.py
+++ app.py
@@ -4,21 +4,7 @@ from flask_login import login_required
 from rotas import ROTAS
 from init import app, db
 
-@app.route('/hello')
-def hello_world():
-    # https://www.youtube.com/watch?v=dwDns8x3Jb4
-    return '<img src=\'https://media1.giphy.com/media/mf8UbIDew7e8g/200.gif\'/>' + \
-        ' '.join(['<p>around the world</p>'] * 144)
-
-
-@login_required
-def rota_inicio():
-
-    return render_template('inicio.html')
-
-
 db.create_all()
-
 
 def adicionar_rotas(rotas):
     '''
@@ -26,9 +12,6 @@ def adicionar_rotas(rotas):
     Quando a rota possui mais de uma opção de cadastramento,
     por exemplo 'view_func' e 'method', esta função adiciona-a 
     mesmo assim.
-
-    Se as 'options' forem um dicionário, ou seja, mais de uma opção.
-    iiiiiii nao terminou a documentação.....
     '''
     for rota, options in rotas.items():
         if type(options) == dict:
@@ -36,13 +19,6 @@ def adicionar_rotas(rotas):
         else:
             app.add_url_rule(rota, view_func=options)
 
-
-outras_rotas = {
-    '/': rota_inicio,
-    '/inicio': rota_inicio,
-}
-
-adicionar_rotas(outras_rotas)
 adicionar_rotas(ROTAS)
 
 if __name__ == '__main__':
