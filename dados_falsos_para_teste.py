0d235799f4f0718a8c6bbb4a2853cc616988dccd
Lemuel Manske <lemuelkaue@gmail.com>
2022-05-23 18:06:03 -0300

    Criando dados "falsos" para testar barra lateral

---
+++ dados_falsos_para_teste.py
@@ -0,0 +1,33 @@
+from modelos import Pagina
+from app import db
+
+import datetime as dt
+
+paginaFavoritaUm = Pagina(nome='paginaFavoritaUm',
+            caminho='',
+            favorito=True,
+            id_usuario=1, 
+            data_criacao=dt.datetime.today(),
+            # excluir_em,
+            )
+
+paginaComumUm = Pagina(nome='paginaComumUm',
+            caminho='',
+            favorito=False,
+            id_usuario=1, 
+            data_criacao=dt.datetime.today(),
+            # excluir_em,
+            )
+
+paginaComumDois = Pagina(nome='paginaComumDois',
+            caminho='',
+            favorito=False,
+            id_usuario=1, 
+            data_criacao=dt.datetime.today(),
+            # excluir_em,
+            )
+
+db.session.add(paginaFavoritaUm)
+db.session.add(paginaComumUm)
+db.session.add(paginaComumDois)
+db.session.commit()
