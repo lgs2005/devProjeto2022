fa3392a4836086bb3d5c9118b7a53c90124bf201
Lemuel Manske <lemuelkaue@gmail.com>
2022-05-23 18:12:24 -0300

    Atualizando barra_lateral.js
    Já está funcionando

--- static/js/barra_lateral.js
+++ static/js/barra_lateral.js
@@ -1,37 +1,24 @@
-$(function () { // quando o documento estiver pronto/carregado
+$(function () {
 
     $.ajax({
-        url: 'listar_paginas', // qual url????
+        url: '/listar_paginas',
         method: 'GET',
-        dataType: 'json', // os dados são recebidos no formato json
-        success: listar_paginas, // chama a função listar para processar o resultado
+        dataType: 'json',
+        success: createPagesList,
         error: function () {
             alert("Erro ao ler dados, verifique o backend");
         }
     });
-
-    /** @param {Array} paginas */
-    function listar_paginas(paginas) {
-        let favoritos = []
-        let outras = []
-
-        for (var pagina of paginas) {
-            if (pagina.favorito) {
-                favoritos.push(pagina)
+    
+    function createPagesList(paginas) {
+        for (var i in paginas) {
+            if (paginas[i].favorito) {
+                lin = '<li class="mb-2">' + paginas[i].nome + '</li>';
+                $("#listaPaginasFavoritas").append(lin);
+            } else {
+                lin = '<li class="mb-2">' + paginas[i].nome + '</li>';
+                $("#listaPaginasComuns").append(lin);
             }
-            else {
-                outras.push(pagina)
-            }
-        }
-
-        listar_paginas(privadas, "#corpoTabelaPaginasPrivadas")
-        listar_paginas(outras, "#corpoTabelaOutrasPaginas")
-    }
-
-    function listar_paginas(paginas, idLista) {
-        for (var pagina of paginas) {
-            lin = `<tr><td>${pagina.nome}</td></tr>`;
-            $(idLista).append(lin);
         }
     }
 });
