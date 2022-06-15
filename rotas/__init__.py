a3ffad63e962e543028d579f1d10e0a692d42700
lucas <lgs22264@gmail.com>
2022-06-15 11:06:42 -0300

    rotas para pegar/publicar e criar páginas falta adicionar uma para deletar

--- rotas/__init__.py
+++ rotas/__init__.py
@@ -8,5 +8,5 @@ ROTAS = {
     **rotas.usuario.adicionar_rotas(),
     **rotas.listar_paginas.adicionar_rotas(),
     **rotas.inicio.adicionar_rotas(),
-    #**rotas.conteudo.adicionar_rotas(),
+    **rotas.conteudo.adicionar_rotas(),
 }
--- rotas/conteudo.py
+++ rotas/conteudo.py
@@ -1,34 +1,15 @@
-from http.client import OK, UNAUTHORIZED, INTERNAL_SERVER_ERROR, NOT_FOUND
+from http.client import GONE, OK, UNAUTHORIZED, INTERNAL_SERVER_ERROR, NOT_FOUND
 
 from flask import make_response, request, abort
 from flask_login import current_user
 
-from modelos import Usuario, Pagina, Compartilhamento
-from paginas import reservar_arquivo
+from modelos import Pagina, Compartilhamento
+from paginas import caminho_para_pagina, reservar_arquivo
 from rotas.utils import requer_login, validar_objeto
 from init import db
 
-def requerir_acesso(usuario: Usuario, pagina: Pagina):
-    '''
-    Requerir acesso à uma página.
-
-    Recebe Usuario e Página.
-    Retorna "Acesso não autorizado", caso não existe um 
-    compartilhamento de páginas.
-    '''
-    if pagina.id_usuario != usuario.id:
-        compartilhamento = Compartilhamento.query \
-            .filter_by(usuario=usuario, pagina=pagina).first()
-        if compartilhamento == None:
-            abort(UNAUTHORIZED, 'Você não tem acesso a esta página.')
-    return True
-
-#       #
-# ROTAS #
-#       #
-
 @requer_login
-def rota_criar_pagina():
+def rota_api_criar_pagina():
     dados = validar_objeto(request.get_json(), {
         'nome': str
     })
@@ -47,60 +28,48 @@ def rota_criar_pagina():
     return OK
 
 @requer_login
-def rota_conteudo(id: int = None):
-    '''
-    documentação FODA
-    '''
+def rota_api_conteudo(id: int = None):
     pagina: Pagina = Pagina.query.get_or_404(id)
-    requerir_acesso(current_user, pagina)
+    
+    # conferir se o usuário tem acesso
+    if pagina.id_usuario != current_user.id:
+        compartilhamento = Compartilhamento.query \
+            .filter_by(usuario=current_user, pagina=pagina).first()
 
-    arquivo_pagina = abrir_pagina(pagina, 'r')
-    conteudo = arquivo_pagina.read()
+        if compartilhamento == None:
+            abort(UNAUTHORIZED)
 
-    return conteudo
+    caminho = caminho_para_pagina(pagina.caminho)
 
+    if request.method == "GET":
+        try:
+            arquivo_pagina = open(caminho, 'r')
+            return arquivo_pagina.read()
+        except FileNotFoundError:
+            abort(NOT_FOUND)
+        except OSError:
+            abort(INTERNAL_SERVER_ERROR)
 
-def rota_publicar_conteudo(id: int = None):
-    '''
-    Rota publicar conteúdo (salvar conteúdo).
-
-    Recebe id: inteiro.
-    Modo leitura -> Write.
-    Se o usuário não tiver acesso a esta página,
-    é retornado um erro 404.
-    '''
-    usuario = current_user
-    pagina: Pagina = Pagina.query.get_or_404(id)
-
-    requerir_acesso(usuario, pagina)
-
-    arquivo_pagina = abrir_pagina(pagina, 'w')
-    dados = request.get_data().decode()
-
-    arquivo_pagina.write(dados)
-    return make_response('Sucesso', OK)
-
+    elif request.method == "POST":
+        try:
+            arquivo_pagina = open(caminho, 'w')
+            arquivo_pagina.truncate()
+            arquivo_pagina.write(request.get_data().decode('utf-8', 'ignore'))
+            return OK
+        except FileNotFoundError:
+            abort(NOT_FOUND)
+        except OSError:
+            abort(INTERNAL_SERVER_ERROR)
 
 def adicionar_rotas():
     return {
-        '/retornar_conteudo/<int:id>': rota_retornar_conteudo,
-        '/publicar_conteudo/<int:id>': {
-            'view_func': rota_publicar_conteudo,
-            'methods': ['POST']
+        '/api/criar_pagina': {
+            'methods': ["POST"],
+            'view_func': rota_api_criar_pagina,
+        },
+
+        '/api/conteudo': {
+            'methods': ["GET", "POST"],
+            'view_func': rota_api_conteudo,
         }
     }
-
-def abrir_pagina(pagina: Pagina, modo: str):
-    '''
-    Abrir página.
-    
-    Recebe Página e modo (write ou read).
-    '''
-    try:
-        return open(f'{PASTA_DE_PAGINAS}/{pagina.caminho}.json', mode=modo)
-    except FileNotFoundError:
-        if modo == 'w':
-            return abrir_pagina(pagina, 'x')
-        abort(NOT_FOUND)
-    except OSError:
-        abort(INTERNAL_SERVER_ERROR)
