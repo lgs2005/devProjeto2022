66b0fda77b41115a9c87bfa17ca0a2155b953050
Lemuel Manske <lemuelkaue@gmail.com>
2022-06-23 17:27:24 -0300

    Update inicio.py

--- rotas/inicio.py
+++ rotas/inicio.py
@@ -5,11 +5,8 @@ from flask_login import current_user
 def rota_default():
     """
     Rota default.
-    Esta rota só existe para o usuário poder acessar o site normalmente 
-    sem ter "POR FAVOR ENTRE EM SUA CONTA PARA VISUALIZAR ESTA PÁGINA" 
-    na sua primeira vez.
     """
-    return render_template('inicio.html') if current_user.is_authenticated else redirect('login')
+    return render_template('inicio.html') if current_user.is_authenticated else redirect('/login')
 
 
 def adicionar_rotas():
