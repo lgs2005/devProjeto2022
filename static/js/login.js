488d19f2c9704b88afe45ca28ac697432bc45325
lucas <no>
2022-07-06 10:03:52 -0300

    lemuel da push logo feiooooo

--- static/js/login.js
+++ static/js/login.js
@@ -12,36 +12,6 @@ const camposForm = {
 	}
 }
 
-function pegarValores(...campos) {
-	return campos.map((id) => $(id).val())
-}
-
-function mostrarErro(campo, erro) {
-	$(campo).toggleClass("is-invalid", true)
-	$(campo + "-erro").text(erro)
-}
-
-function testarPara(campo, teste) {
-	erro = teste();
-	if (erro != undefined) {
-		mostrarErro(campo, erro)
-		return true
-	} else {
-		return false
-	}
-}
-
-function camposPreenchidos(...campos) {
-	let preenchidos = true
-	for (let campo of campos) {
-		if ($(campo).val().toString() == "") {
-			mostrarErro(campo, "Preencha o campo.")
-			preenchidos = false
-		}
-	}
-	return preenchidos
-}
-
 jQuery(function($) {
 
 	$(".form-control").on("input", function() {
