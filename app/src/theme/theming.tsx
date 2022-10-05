fb9a1178365f0f76d49fd8beec2db59f25b96618
Manske <lemuelkaue@gmail.com>
2022-10-05 09:55:10 -0300

    Adicionando temas

---
+++ app/src/theme/theming.tsx
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#FBF5FC',
+			contrastText: '#0F0F42',
+		},
+		secondary: {
+			main: '#E2DCE3',
+		},
+	}
+})
